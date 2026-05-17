18bd0fb97f7aa579eaebd966b7385148daa58334
mehedihasan3000 <mehedihasan3000@protonmail.com>
2026-05-17 23:32:19 +0600

    connect with db

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 3000
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("tutor-booking");
+        const movies = db.collection("tutors");
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
